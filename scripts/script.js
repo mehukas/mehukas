9773e5b2411bcb2ef997c48e1e0ce72fcf8e837d
Colin Alexander <colin.1.alexander@gmail.com>
2017-01-04 09:17:43 -0800

    map fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
+function myMap() {
+    var uluru = {lat: 60.4157266, lng: 25.7952598};  
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
+
 function activateButton(button) {
   $(button).addClass('active').siblings().removeClass('active');
 }
@@ -36,36 +48,3 @@ $(function() {
     switchLanguage(this, "EN-fi");
   });
 });
-
-function myMap() {
-  /*
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(60.4157266, 25.7952,598),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-  */
-  
-    var uluru = {lat: 60.4157266, lng: 25.7952,598};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-}
-
-function initMap() {
-        var uluru = {lat: 60.4157266, lng: 25.7952,598};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
