30ee584d68fd2ea5070a6f2c7771012dcf42fdc5
mehukas <jenny.ekholm@gmail.com>
2018-09-01 12:01:21 -0700

    Cash only and added address & coordinates

---
+++ script.js
@@ -0,0 +1,56 @@
+// https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+// https://console.developers.google.com/iam-admin/settings/project?project=mehukas-154611
+// https://console.developers.google.com/apis/api/maps_backend/overview?project=mehukas-154611&duration=P30D
+
+function myMap() {
+    var coordinates = {
+      lat: 60.415725,
+      lng: 25.7798792};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: coordinates
+    });
+    var marker = new google.maps.Marker({
+      position: coordinates,
+      map: map
+    });
+}
+
+function activateButton(button) {
+  $(button).addClass('active').siblings().removeClass('active');
+}
+
+
+function switchLanguage(button, language) {
+  document.documentElement.lang = language;
+  activateButton(button)
+
+}
+
+$(function() {
+  $("#appleLogo").click(function() {
+    $("#btn_about").addClass('active').siblings().removeClass('active');
+  });
+  $("#mehukas").click(function() {
+    $("#btn_about").addClass('active').siblings().removeClass('active');
+  });
+  $("#btn_about").click(function() {
+    activateButton(this);
+  });
+  $("#btn_prices").click(function() {
+    activateButton(this);
+  });
+  $("#btn_contact").click(function() {
+    activateButton(this);
+  });
+
+  $("#btn_finnish").click(function() {
+    switchLanguage(this, "FI-fi");
+  });
+  $("#btn_swedish").click(function() {
+    switchLanguage(this, "SV-fi");
+  });
+  $("#btn_english").click(function() {
+    switchLanguage(this, "EN-fi");
+  });
+});
